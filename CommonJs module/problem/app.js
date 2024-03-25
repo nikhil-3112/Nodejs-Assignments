b9f13c43523d17da60963985b628bdb6db3b7339
Nikhil Chaudhari <nikhil.l.c31@outlook.com>
2024-03-25 09:01:46 +0530

    CommonJs module

---
+++ CommonJs module/problem/app.js
@@ -0,0 +1,14 @@
+// Note:  Please do not change the pre-written code
+
+// import the required module here
+const math = require('./math');
+
+
+const Solution = () => {
+    const nums = [1, 2, 3, 4, 5];
+    console.log(math.sum(nums));
+    console.log(math.mean(nums));
+
+};
+Solution();
+module.exports = Solution;
