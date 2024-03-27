35ad86f063c85be4682a9e9abc1bf61960014f40
Nikhil Chaudhari <nikhil.l.c31@outlook.com>
2024-03-27 21:17:01 +0530

    Fitness Tracker Application

--- File Appending and Printing/problem/index.js
+++ File Appending and Printing/problem/index.js
@@ -1,34 +1,23 @@
+// Please do not change the prewritten code
+
 import http from "http";
 import fs from "fs";
 
 const server = http.createServer((req, res) => {
-  if (req.method == 'POST') {
-    let body = '';
+  //  Write your code here
+  if(req.method=='POST'){
+    //Expecting data from client
+    let data = ''
+    req.on('data', (chunk)=>{
+        data+=chunk.toString()
 
-    req.on('data', (chunk) => {
-      body += chunk.toString();
     });
-
-    req.on('end', () => {
-      try {
-        const requestBody = JSON.parse(body);
-        console.log(requestBody);
-
-        // Write to file
-        fs.appendFileSync("data.txt", JSON.stringify(requestBody));
-
-        // Read file and log contents
-        console.log(fs.readFileSync("data.txt", "utf-8"));
-      } catch (error) {
-        console.error('Error parsing JSON:', error);
-      }
+    req.on('end', ()=>{
+        fs.appendFileSync('data.txt',data);
+        console.log(fs.readFileSync('data.txt','utf-8'))
     });
+    res.end('Data is received')
   }
-
-  res.end("data received");
 });
-
-server.listen(3100);
-console.log("Server is running on Port 3100.");
 
 export default server;
